c4c0d102d06034402337b290647527bf9ff0af1c
Dave Ubuntu <dboland77@gmail.com>
2024-07-17 09:24:37 +0100

    removed styled polygon and unused components from home view

--- src/sections/home/view/home-view.tsx
+++ src/sections/home/view/home-view.tsx
@@ -7,45 +7,13 @@ import { styled } from '@mui/material/styles';
 
 import MainLayout from 'src/layouts/main';
 import { ContactView } from 'src/sections/contact/view';
-import FormValidationView from 'src/sections/form-validation-view'
 
 import ScrollProgress from 'src/components/scroll-progress';
 
-// import HomeHero from '../home-hero';
 import HomeEnabled from '../home-enabled';
 import HomeFeatures from '../home-features';
 import HomeFAQ from '../home-faq';
-// import HomePricing from '../home-pricing';
-// import HomeDarkMode from '../home-dark-mode';
-// import HomeLookingFor from '../home-looking-for';
-// import HomeForDesigner from '../home-for-designer';
-// import HomeColorPresets from '../home-color-presets';
-// import HomeAdvertisement from '../home-advertisement';
-// import HomeCleanInterfaces from '../home-clean-interfaces';
-
-type StyledPolygonProps = {
-  anchor?: 'top' | 'bottom';
-};
-
-const StyledPolygon = styled('div')<StyledPolygonProps>(({ anchor = 'top', theme }) => ({
-  left: 0,
-  zIndex: 9,
-  height: 80,
-  width: '100%',
-  position: 'absolute',
-  clipPath: 'polygon(0% 0%, 100% 100%, 0% 100%)',
-  backgroundColor: theme.palette.background.default,
-  display: 'block',
-  lineHeight: 0,
-  ...(anchor === 'top' && {
-    top: -1,
-    transform: 'scale(-1, -1)',
-  }),
-  ...(anchor === 'bottom' && {
-    bottom: -1,
-    backgroundColor: theme.palette.grey[900],
-  }),
-}));
+import HomeForDesigner from '../home-for-designer';
 
 export default function HomeView() {
   const { scrollYProgress } = useScroll();
@@ -54,7 +22,6 @@ export default function HomeView() {
     <MainLayout>
       <ScrollProgress scrollYProgress={scrollYProgress} />
 
-      {/* <HomeHero /> */}
 
       <Box
         sx={{
@@ -63,31 +30,12 @@ export default function HomeView() {
           bgcolor: 'background.default',
         }}
       >
-        <FormValidationView/>
         <HomeEnabled />
         <HomeFeatures />
         <HomeFAQ />
         <ContactView />
 
-        {/* <HomeHugePackElements /> */}
 
-        <Box sx={{ position: 'relative' }}>
-          <StyledPolygon />
-          {/* <HomeForDesigner /> */}
-          <StyledPolygon anchor="bottom" />
-        </Box>
-
-        {/* <HomeDarkMode /> */}
-
-        {/* <HomeColorPresets /> */}
-
-        {/* <HomeCleanInterfaces /> */}
-
-        {/* <HomePricing /> */}
-
-        {/* <HomeLookingFor /> */}
-
-        {/* <HomeAdvertisement /> */}
       </Box>
     </MainLayout>
   );
