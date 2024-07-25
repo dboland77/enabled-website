4a7ad879e408bc5ee6c1cec4b65ba9da1d0fa968
Dave Ubuntu <dboland77@gmail.com>
2024-07-25 13:31:31 +0100

    styled feature cards and fonts

--- src/sections/home/home-features.tsx
+++ src/sections/home/home-features.tsx
@@ -1,11 +1,8 @@
-import { m } from 'framer-motion';
-
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import { alpha } from '@mui/material/styles';
+import { CardContent } from '@mui/material';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-
-import { varFade } from 'src/components/animate';
 
 const FEATURES = [
   {
@@ -24,67 +21,64 @@ const FEATURES = [
   {
     icon: '/assets/icons/home/ic_development.svg',
     title: 'Compliance Tracking',
-    description: `enableD helps employers stay compliant with disability laws and regulations. 
-    
-    We provide easy-to-use tools that allow you to track adjustments, making it simple to stay on top of your legal obligations.`,
+    description: `enableD helps employers comply with disability laws by providing easy-to-use tools for tracking adjustments, ensuring they stay on top of their legal obligations.`,
   },
   {
     icon: '/assets/icons/home/ic_make_brand.svg',
     title: 'Data Analytics',
-    description: `Our system allows employers to track and analyse adjustment requests, providing valuable insights into employee needs and preferences.`,
+    description: `enableD allows employers to track and analyse adjustment requests, providing valuable insights into employee needs and preferences.`,
   },
   {
     icon: '/assets/icons/home/ic_design.svg',
-    title: 'Personal Adjustment Passport.',
-    description: `enableD allows employees to build up a profile of adjustments that work for them and their unique needs. 
-    
-    This profile can be exported and used wherever it is needed - not just in the workplace.`,
+    title: 'Adjustments Passport.',
+    description: `enableD lets employees create a profile of adjustments tailored to their unique needs. This profile can be exported and used beyond the workplace.`,
   },
 ];
 
 export default function HomeFeatures() {
   return (
-    <Box
-      gap={{ xs: 5, lg: 10 }}
-      display="grid"
-      alignItems="center"
-      gridTemplateColumns={{
-        xs: 'repeat(1, 1fr)',
-        md: 'repeat(3, 1fr)',
-      }}
-    >
-      {FEATURES.map((feature, index) => (
-        <m.div variants={varFade().inUp} key={feature.title}>
+    <Container sx={{ mb: 3 }}>
+      <Box
+        gap={{ xs: 5, lg: 10 }}
+        display="grid"
+        alignItems="center"
+        gridTemplateColumns={{
+          xs: 'repeat(1, 1fr)',
+          md: 'repeat(3, 1fr)',
+        }}
+      >
+        {FEATURES.map((feature, index) => (
           <Card
             sx={{
+              mx: {
+                minHeight: 420,
+              },
               textAlign: 'left',
               bgcolor: 'background.default',
-              p: (theme) => theme.spacing(5, 5),
+              p: (theme) => theme.spacing(2, 5),
 
               boxShadow: (theme) => ({
-                md: `-40px 40px 80px ${
-                  theme.palette.mode === 'light'
-                    ? alpha(theme.palette.grey[500], 0.16)
-                    : alpha(theme.palette.common.black, 0.4)
-                }`,
+                md: `-30px 30px 80px`,
               }),
             }}
           >
-            <Box
-              component="img"
-              src={feature.icon}
-              alt={feature.title}
-              sx={{ mx: 'auto', width: 48, height: 48 }}
-            />
+            <CardContent>
+              <Box
+                component="img"
+                src={feature.icon}
+                alt={feature.title}
+                sx={{ mx: 'auto', width: 48, height: 48 }}
+              />
 
-            <Typography variant="h5" sx={{ mt: 8, mb: 2 }}>
-              {feature.title}
-            </Typography>
+              <Typography variant="h5" sx={{ mt: 3, mb: 2 }}>
+                {feature.title}
+              </Typography>
 
-            <Typography sx={{ color: 'text.primary' }}>{feature.description}</Typography>
+              <Typography sx={{ color: 'text.primary' }}>{feature.description}</Typography>
+            </CardContent>
           </Card>
-        </m.div>
-      ))}
-    </Box>
+        ))}
+      </Box>
+    </Container>
   );
 }
