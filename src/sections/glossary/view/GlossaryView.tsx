34771e09aeec8a17759f26d16597ce60753481fd
Dave Ubuntu <dboland77@gmail.com>
2025-04-25 10:31:19 +0100

    adding virtualisation to glossary

--- src/sections/glossary/view/GlossaryView.tsx
+++ src/sections/glossary/view/GlossaryView.tsx
@@ -1,61 +1,59 @@
 'use client';
-
-// import { useState } from 'react';
 import Container from '@mui/material/Container';
-
 import MainLayout from 'src/layouts/main';
-import Drawer from '@mui/material/Drawer';
 import Glossary from '../Glossary';
-import GlossaryNav from './GlossaryNav';
+
+import { VariableSizeGrid as Grid } from 'react-window';
+
+
+// These cell sizes are arbitrary.
+// Yours should be based on the content of the cell.
+const columnWidths = new Array(1000)
+  .fill(true)
+  .map(() => 75 + Math.round(Math.random() * 50));
+const rowHeights = new Array(1000)
+  .fill(true)
+  .map(() => 25 + Math.round(Math.random() * 50));
+
+const Cell = ({ columnIndex, rowIndex, style }) => (
+  <div
+    className={
+      columnIndex % 2
+        ? rowIndex % 2 === 0
+          ? 'GridItemOdd'
+          : 'GridItemEven'
+        : rowIndex % 2
+        ? 'GridItemOdd'
+        : 'GridItemEven'
+    }
+    style={style}
+  >
+    r{rowIndex}, c{columnIndex}
+  </div>
+);
+
+const Example = () => (
+  <Grid
+    className="Grid"
+    columnCount={1000}
+    columnWidth={index => columnWidths[index]}
+    height={150}
+    rowCount={1000}
+    rowHeight={index => rowHeights[index]}
+    width={300}
+  >
+    {Cell}
+  </Grid>
+);
+
 
 export default function FeaturesView() {
-  const drawerWidth = 240;
-  // const [mobileOpen, setMobileOpen] = useState(false);
-  // const [isClosing, setIsClosing] = useState(false);
 
-  // const handleDrawerClose = () => {
-  //   setIsClosing(true);
-  //   setMobileOpen(false);
-  // };
-
-  // const handleDrawerTransitionEnd = () => {
-  //   setIsClosing(false);
-  // };
-
-  // const handleDrawerToggle = () => {
-  //   if (!isClosing) {
-  //     setMobileOpen(!mobileOpen);
-  //   }
-  // };
   return (
     <MainLayout>
       <Container id="glossary">
-        <Drawer
-          variant="temporary"
-          // open={mobileOpen}
-          // onTransitionEnd={handleDrawerTransitionEnd}
-          // onClose={handleDrawerClose}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          <GlossaryNav />
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          <GlossaryNav />
-        </Drawer>
-        <Glossary />
+        {/* <Glossary /> */}
+        <Example/>
       </Container>
     </MainLayout>
   );
